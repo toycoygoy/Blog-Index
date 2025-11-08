0f3aa898ed13a7eb92612c6151f5512b37d248f0
toycoygoy <toycoygoy@gmail.com>
2025-11-08 11:11:44 +0000

    Edit the site title and add two links in the CenterLogo component.

--- src/config.ts
+++ src/config.ts
@@ -298,7 +298,7 @@ const GLOBAL_CONFIG = {
    * 网站 Title Keywords Description 的配置，用于 SEO
    */
   TKD: {
-    title: "My Blog Index",
+    title: "Idk's Landing Page",
     keywords: "Blog, Index, Index Page",
     description: "This is my personal blog index page.",
   },
